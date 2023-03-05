1213cac04adb61afe709c978120ce6943cfce4b8
Francisco Pilosio <fjpilosio@gmail.com>
2023-03-05 12:29:33 +0100

    maquetado

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,16 @@
-<template>
-  <div class="h-40 w-40 bg-red-500"></div>
-</template>
+<template><HsLayout /></template>
 
 <script>
-import { useStoryblok } from '@storyblok/vue';
+import HsLayout from "../layout/index.vue";
+import { useStoryblok } from "@storyblok/vue";
 export default {
-  name: 'Home',
+  components: { HsLayout },
+  name: "Home",
   setup() {
-    (async ()=> {
-      const story = await useStoryblok('home', { version: 'draft' })
+    (async () => {
+      const story = await useStoryblok("home", { version: "draft" });
       console.log(story);
-    })()
-  }
-}
+    })();
+  },
+};
 </script>
